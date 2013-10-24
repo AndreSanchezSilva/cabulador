30f9244e5743c9bf1caf1959297f63c08dbfc7a0
AndreSanchezSilva <andre@worldimportados.com.br>
2013-10-23 22:20:59 -0200

    calculo de media e classe

--- app.js
+++ app.js
@@ -2,34 +2,104 @@ angular.module('NotasApp', ['ui.bootstrap']);
 function NotasAppCrl($scope) {
   $scope.oneAtATime = true;
 
-  $scope.materias = [
+  $scope.materias2 = [
     {
-      title: "Programação II",
+      config: {
+        nome: "Programação II",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     },
     {
-      title: "Banco de Dados I",
+      config: {
+        nome: "Banco de Dados I",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     },
     {
-      title: "Design Gráfico II",
+      config: {
+        nome: "Design de Interação",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     },
     {
-      title: "Design de Interação",
+      config: {
+        nome: "Engenharia de Software I",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     },
     {
-      title: "Engenharia de Software I",
+      config: {
+        nome: "Formação Sociocultural e Ética",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     },
     {
-      title: "Formação Sociocultural e Ética",
+      config: {
+        nome: "Design de Interação",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     },
     {
-      title: "Multimídia para a Internet",
+      config: {
+        nome: "Multimídia para a Internet",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: ['','','',''],
+      notasSub: ['',''],
+      faltas: 0
     }
   ];
+  $scope.calcularMedia = function ( materia ) {
 
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+    var nota1 = materia.notasBim[0]!=''?parseFloat(materia.notasBim[0]):0;
+    var nota2 = materia.notasBim[1]!=''?parseFloat(materia.notasBim[1]):0;
+    var nota3 = materia.notasBim[2]!=''?parseFloat(materia.notasBim[2]):0;
+    var nota4 = materia.notasBim[3]!=''?parseFloat(materia.notasBim[3]):0;
 
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
+    var media =  (nota1+nota2+nota3+nota4)-240 ;
+
+    return media;
+  }
+  $scope.classMedia = function ( materia ) {
+    switch (true) {
+      case (this.calcularMedia(materia)>0):
+          return 'badge-success'
+        break;
+      default:
+          return 'badge-important'
+        break;
+    }
+  }
 }
