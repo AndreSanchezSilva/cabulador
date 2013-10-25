bd40334e4ae47f795011ad4453f64cdd0379ead6
Andre Silva <andre@worldimportados.com.br>
2013-10-25 15:24:43 -0200

    arrumado bug quando uma nota era null

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ function NotasAppCrl($scope) {
         notaMinima: 240,
         bimestral: true
       },
-      notasBim: ['','','',''],
+      notasBim: [10,10,9.4,null],
       notasSub: ['',''],
-      faltas: 0
+      faltas: 34
     },
     {
       config: {
@@ -21,42 +21,9 @@ function NotasAppCrl($scope) {
         notaMinima: 240,
         bimestral: true
       },
-      notasBim: ['','','',''],
+      notasBim: [7.5,8.3,9.1,null],
       notasSub: ['',''],
-      faltas: 0
-    },
-    {
-      config: {
-        nome: "Design de Interação",
-        faltasPermitidas: 40,
-        notaMinima: 240,
-        bimestral: true
-      },
-      notasBim: ['','','',''],
-      notasSub: ['',''],
-      faltas: 0
-    },
-    {
-      config: {
-        nome: "Engenharia de Software I",
-        faltasPermitidas: 40,
-        notaMinima: 240,
-        bimestral: true
-      },
-      notasBim: ['','','',''],
-      notasSub: ['',''],
-      faltas: 0
-    },
-    {
-      config: {
-        nome: "Formação Sociocultural e Ética",
-        faltasPermitidas: 40,
-        notaMinima: 240,
-        bimestral: true
-      },
-      notasBim: ['','','',''],
-      notasSub: ['',''],
-      faltas: 0
+      faltas: 34
     },
     {
       config: {
@@ -65,7 +32,40 @@ function NotasAppCrl($scope) {
         notaMinima: 240,
         bimestral: true
       },
-      notasBim: ['','','',''],
+      notasBim: [4,8.3,8,null],
+      notasSub: [6,''],
+      faltas: 16
+    },
+    {
+      config: {
+        nome: "Design de Gráfico II",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: [7.7,6,6.5,null],
+      notasSub: ['',''],
+      faltas: 32
+    },
+    {
+      config: {
+        nome: "Engenharia de Software I",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: [7.4,6.8,7.2,null],
+      notasSub: ['',''],
+      faltas: 22
+    },
+    {
+      config: {
+        nome: "Sociocultural e Ética",
+        faltasPermitidas: 40,
+        notaMinima: 240,
+        bimestral: true
+      },
+      notasBim: [5.3,9,null,null],
       notasSub: ['',''],
       faltas: 0
     },
@@ -76,27 +76,31 @@ function NotasAppCrl($scope) {
         notaMinima: 240,
         bimestral: true
       },
-      notasBim: ['','','',''],
+      notasBim: [8.5,8.6,10,null],
       notasSub: ['',''],
-      faltas: 0
+      faltas: 12
     }
   ];
 
   $scope.calcularMedia = function ( materia ) {
 
-    var nota1 = materia.notasBim[0]!=''?parseFloat(materia.notasBim[0]):0;
-    var nota2 = materia.notasBim[1]!=''?parseFloat(materia.notasBim[1]):0;
-    var nota3 = materia.notasBim[2]!=''?parseFloat(materia.notasBim[2]):0;
-    var nota4 = materia.notasBim[3]!=''?parseFloat(materia.notasBim[3]):0;
+    var nota1 = materia.notasBim[0]!=null?parseFloat(materia.notasBim[0]):0;
+    var nota2 = materia.notasBim[1]!=null?parseFloat(materia.notasBim[1]):0;
+    var nota3 = materia.notasBim[2]!=null?parseFloat(materia.notasBim[2]):0;
+    var nota4 = materia.notasBim[3]!=null?parseFloat(materia.notasBim[3]):0;
 
-    var media =  (nota1+nota2+nota3+nota4)-240 ;
+    var media =  (nota1+nota2+nota3+nota4)-24;
+    media = media.toFixed(1);
+
+    if (media>0)
+    	media = '+' + media;
 
     return media;
   }
 
   $scope.classMedia = function ( materia ) {
     switch (true) {
-      case (this.calcularMedia(materia)>0):
+      case (this.calcularMedia(materia)>=0):
           return 'badge-success'
         break;
       default:
